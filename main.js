283d842cb0ba57561772361f576fefee9886fee4
katynightshade <98486819+katynightshade@users.noreply.github.com>
2022-05-02 15:21:10 -0600

    html set up, attempt at addBook function

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 const textLine = document.getElementById('textline');
+let myLibrary = [];
+let newBook;
 
 function Books(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        return `${title} is written by ${author} and is ${pages} long. You have ${read} this book.`
-    }
+}
+
+Books.prototype.info = function() {
+    return (this.title + ' is written by ' + this.author + ' and is ' + this.pages + ' long. You have ' + this.read + ' this book.')
+}
+
+function addBooks() {
+    newBook = new Books(title, author, pages, read);
+    myLibrary.push(newBook);
 }
 
 let theHobbit = new Books('The Hobbit', 'JRR Tolkien', '304 pages', 'read');
 let hpSix = new Books('Harry Potter and the Half-Blood Prince', 'JK Rowling', '607 pages', 'not read');
-
-console.log(hpSix.info());
-
-//Not sure how to use object contructors quite yet, currently attempting to console log '(book title) is written by (author) and is (pages) long. you have (read/not read) this book.'.
